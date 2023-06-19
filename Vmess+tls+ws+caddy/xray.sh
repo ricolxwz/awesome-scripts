7599d9845cc0bafe4482b0f2484d593d29cc196a
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-19 22:12:50 +0800

    Update xray.sh

--- Vmess+tls+ws+caddy/xray.sh
+++ Vmess+tls+ws+caddy/xray.sh
@@ -3,64 +3,64 @@ read -p "Enter uuid: " uuid
 read -p "Enter email: " email
 read -p "Enter Websocket path: " path
 read -p "Enter xray port: " port
-echo "{
-  "log": {
-    "loglevel": "warning",
-    "error": "/var/log/xray/error.log",
-    "access": "/var/log/xray/access.log"
-  },
-  "inbounds": [
-    {
-      "listen": "127.0.0.1",
-      "port": $port,
-      "protocol": "vmess",
-      "settings": {
-        "clients": [
-          {
-            "id": "$uuid"
-            "email": "$email"
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws",
-        "security": "none",
-        "wsSettings": {
-          "path": "$path"
-        }
-      },
-      "sniffing": {
-        "enabled": true,
-        "destOverride": [
-          "http",
-          "tls"
-        ]
-      }
-    }
-  ],
-  "routing": {
-    "rules": [
+echo -e "{
+    \x22log\x22: {
+      \x22loglevel\x22: \x22warning\x22,
+      \x22error\x22: \x22/var/log/xray/error.log\x22,
+      \x22access\x22: \x22/var/log/xray/access.log\x22
+    },
+    \x22inbounds\x22: [
       {
-        "type": "field",
-        "protocol": [
-          "bittorrent"
-        ],
-        "outboundTag": "blocked"
+        \x22listen\x22: \x22127.0.0.1\x22,
+        \x22port\x22: $port,
+        \x22protocol\x22: \x22vmess\x22,
+        \x22settings\x22: {
+          \x22clients\x22: [
+            {
+              \x22id\x22: \x22$uuid\x22
+              \x22email\x22: \x22$email\x22
+            }
+          ]
+        },
+        \x22streamSettings\x22: {
+          \x22network\x22: \x22ws\x22,
+          \x22security\x22: \x22none\x22,
+          \x22wsSettings\x22: {
+            \x22path\x22: \x22$path\x22
+          }
+        },
+        \x22sniffing\x22: {
+          \x22enabled\x22: true,
+          \x22destOverride\x22: [
+            \x22http\x22,
+            \x22tls\x22
+          ]
+        }
+      }
+    ],
+    \x22routing\x22: {
+      \x22rules\x22: [
+        {
+          \x22type\x22: \x22field\x22,
+          \x22protocol\x22: [
+            \x22bittorrent\x22
+          ],
+          \x22outboundTag\x22: \x22blocked\x22
+        }
+      ]
+    },
+    \x22outbounds\x22: [
+      {
+        \x22protocol\x22: \x22freedom\x22,
+        \x22settings\x22: {}
+      },
+      {
+        \x22tag\x22: \x22blocked\x22,
+        \x22protocol\x22: \x22blackhole\x22,
+        \x22settings\x22: {}
       }
     ]
-  },
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    },
-    {
-      "tag": "blocked",
-      "protocol": "blackhole",
-      "settings": {}
-    }
-  ]
-}" > /usr/local/etc/xray/config.json
+  }" > /usr/local/etc/xray/config.json
 apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
