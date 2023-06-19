96d2ff50237f47bc2684b9fe7aa8070dd146bd49
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-19 18:26:34 +0800

    Update 1.sh

--- Naiveproxy/1.sh
+++ Naiveproxy/1.sh
@@ -30,3 +30,7 @@ npm install pm2 -g
 pm2 start ./caddy -n caddy -- run --config Caddyfile
 pm2 save
 pm2 startup
+sed -i '/1.sh/d' /etc/rc.local
+sed -i '/root/a ./2.sh' /etc/rc.local
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
+reboot
