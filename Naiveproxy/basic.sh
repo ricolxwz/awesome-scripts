337db9e9b88ce9e24834e259ffead00f0576d05d
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-19 19:31:10 +0800

    Update basic.sh

--- Naiveproxy/basic.sh
+++ Naiveproxy/basic.sh
@@ -1,11 +1,11 @@
 apt update -y
 apt install wget curl sudo -y
 cd /root
-wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/1.sh
-wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/2.sh
-wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/3.sh
-wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/4.sh
-chmod 777 1.sh
-chmod 777 2.sh
-chmod 777 3.sh
-chmod 777 4.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/network.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/naive.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/bbr.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/jarm.sh
+chmod 777 network.sh
+chmod 777 naive.sh
+chmod 777 bbr.sh
+chmod 777 jarm.sh
