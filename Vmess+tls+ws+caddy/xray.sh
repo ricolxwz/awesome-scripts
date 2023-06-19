8cf2993464057bd71d9e63b7b4ae4404639ce59f
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-19 21:09:05 +0800

    Update xray.sh

--- Vmess+tls+ws+caddy/xray.sh
+++ Vmess+tls+ws+caddy/xray.sh
@@ -75,26 +75,22 @@ echo "{
 		level ERROR
 	} 
 	email $email
-
 	servers :443 {
 		protocols h1 h2
 	}
 }
-
 :443, $domain {
 	tls {
 		ciphers TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256 TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
 		curves x25519 secp521r1 secp384r1 secp256r1
 		alpn http/1.1 h2
 	}
-
 	@vmws {
 		path $path
 		header Connection *Upgrade*
 		header Upgrade websocket
 	}
 	reverse_proxy @vmws 127.0.0.1:$port
-
 	@host {
 		host $domain
 	}
