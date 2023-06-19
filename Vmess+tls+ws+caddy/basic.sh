07f0f1dbf469fc0132aea4b33ae19dacd8378efe
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-19 20:19:02 +0800

    Update basic.sh

--- Vmess+tls+ws+caddy/basic.sh
+++ Vmess+tls+ws+caddy/basic.sh
@@ -1,1 +1,8 @@
-apt update -y
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Vmess%2Btls%2Bws%2Bcaddy/network.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Vmess%2Btls%2Bws%2Bcaddy/xray.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Vmess%2Btls%2Bws%2Bcaddy/bbr.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Vmess%2Btls%2Bws%2Bcaddy/clean.sh
+chmod 777 network.sh
+chmod 777 xray.sh
+chmod 777 bbr.sh
+chmod 777 clean.sh
