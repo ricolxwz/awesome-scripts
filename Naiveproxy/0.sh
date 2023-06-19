ab019696f90c024e9743a705ea5bb49359e98554
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-19 19:20:31 +0800

    Update 0.sh

--- Naiveproxy/0.sh
+++ Naiveproxy/0.sh
@@ -5,7 +5,9 @@ wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/1.sh
 wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/2.sh
 wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/3.sh
 wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/4.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/5.sh
 chmod 777 1.sh
 chmod 777 2.sh
 chmod 777 3.sh
 chmod 777 4.sh
+chmod 777 5.sh
