39de3c5ac6989499fd99cdd3a7285ec11b69e41b
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-19 19:15:59 +0800

    Create 0.sh

---
+++ Naiveproxy/0.sh
@@ -0,0 +1,9 @@
+apt update -y
+apt install wget curl sudo -y
+cd /root
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/1.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/2.sh
+wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/3.sh
+chmod 777 1.sh
+chmod 777 2.sh
+chmod 777 3.sh
