910cfc49bd2591c82ae3a21eb23a5bf91c0c4c7b
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-19 19:39:46 +0800

    Create jarm.sh

---
+++ Vmess+tls+ws+caddy/jarm.sh
@@ -0,0 +1,7 @@
+cd /root
+read -p "Please Enter your Domain: " domain
+wget https://raw.githubusercontent.com/salesforce/jarm/master/jarm.py
+python3 jarm.py $domain
+read -p "Please Enter your Hide Domain: " hide
+sed -i "/reverse_proxy/d" Caddyfile
+sed -i "/upstream_hostport/i  reverse_proxy $hide {" Caddyfile
