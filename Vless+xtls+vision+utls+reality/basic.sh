61e43824ae092736fe99309811ac0d3e9d711ea0
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-20 08:34:15 +0800

    Update basic.sh

--- Vless+xtls+vision+utls+reality/basic.sh
+++ Vless+xtls+vision+utls+reality/basic.sh
@@ -1,6 +1,6 @@
-wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Vless%2Bxtls%2Bvision%2Butls%2Breality/network.sh
+wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Common/network.sh
 wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Vless%2Bxtls%2Bvision%2Butls%2Breality/xray.sh
-wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Vless%2Bxtls%2Bvision%2Butls%2Breality/bbr.sh
+wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Common/bbr.sh
 wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Vless%2Bxtls%2Bvision%2Butls%2Breality/clean.sh
 chmod 777 network.sh
 chmod 777 xray.sh
