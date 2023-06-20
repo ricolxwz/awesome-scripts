bb0b6fbf231c478d958f1d2b4cad7d967670c90e
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-20 09:38:49 +0800

    Update xray.sh

--- Vmess+tls+ws+caddy/xray.sh
+++ Vmess+tls+ws+caddy/xray.sh
@@ -67,6 +67,7 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo 
 apt update -y
 apt install caddy
 read -p "Enter domain: " domain
+read -p "Enter caddy port: " caddyport
 echo "{
 	order reverse_proxy before route
 	admin off
@@ -79,7 +80,7 @@ echo "{
 		protocols h1 h2
 	}
 }
-:443, $domain {
+:$caddyport, $domain {
 	tls {
 		ciphers TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256 TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
 		curves x25519 secp521r1 secp384r1 secp256r1
