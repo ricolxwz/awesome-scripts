38c0af6c288a2b7f7ed06a57a00b04a8a4d987e9
Ricol <92409532+Ricolxwz@users.noreply.github.com>
2023-06-20 08:52:57 +0800

    Update basic.sh

--- Naiveproxy/basic.sh
+++ Naiveproxy/basic.sh
@@ -1,8 +1,8 @@
 wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Common/network.sh
-wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/naive.sh
+wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Naiveproxy/naive.sh
 wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Common/bbr.sh
-wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/jarm.sh
-wget https://raw.githubusercontent.com/Ricolxwz/BGFW-sh/master/Naiveproxy/clean.sh
+wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Naiveproxy/jarm.sh
+wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Naiveproxy/clean.sh
 chmod 777 network.sh
 chmod 777 naive.sh
 chmod 777 bbr.sh
