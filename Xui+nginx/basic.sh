ee8681b4e04202e353b658d6d57eaa4cd0246082
从言午声 <92409532+Ricolxwz@users.noreply.github.com>
2023-06-20 11:55:49 +0800

    Update basic.sh

--- Xui+nginx/basic.sh
+++ Xui+nginx/basic.sh
@@ -1,1 +1,2 @@
-
+wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Common/network.sh
+wget https://raw.githubusercontent.com/Ricolxwz/integrated-scripts/master/Common/bbr.sh
