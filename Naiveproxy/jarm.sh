3f9494322112ce0ddec386155177b2c0fdf58b91
从言午声 <92409532+Ricolxwz@users.noreply.github.com>
2023-06-20 13:25:28 +0800

    Update jarm.sh

--- Naiveproxy/jarm.sh
+++ Naiveproxy/jarm.sh
@@ -1,7 +1,7 @@
 read -p "Please Enter your Domain: " domain
 wget https://raw.githubusercontent.com/salesforce/jarm/master/jarm.py
 python3 jarm.py $domain
-read -p "Please Enter your Hide Domain: " hide
+read -p "Please Enter your Hide Domain:(contain https://) " hide
 sed -i "/reverse_proxy/d" Caddyfile
 sed -i "/upstream_hostport/i  reverse_proxy $hide {" Caddyfile
 reboot
