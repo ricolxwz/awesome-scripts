2f646753c74c5e9688fbdd54eac606fcad605415
从言午声 <92409532+Ricolxwz@users.noreply.github.com>
2023-06-20 13:24:17 +0800

    Update jarm.sh

--- Xui+nginx/jarm.sh
+++ Xui+nginx/jarm.sh
@@ -2,7 +2,7 @@ read -p "Please Enter your Domain: " domain
 wget https://raw.githubusercontent.com/salesforce/jarm/master/jarm.py
 python3 jarm.py $domain
 cd /etc/nginx
-read -p "Please Enter your Hide Domain:(do not contain https://) " hide
+read -p "Please Enter your Hide Domain: " hide
 sed -i "/proxy_pass https:\/\/www.bing.com;/a proxy_pass https:\/\/$hide;" nginx.conf
 sed -i "/proxy_pass https:\/\/www.bing.com;/d" nginx.conf
 sed -i "/sub_filter \"www.bing.com\"/a sub_filter \"$hide\" \$server_name;" nginx.conf
