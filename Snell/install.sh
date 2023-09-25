ec08028a4d9a4b163e1e5014009f1c4d1ce7a08a
McflurryTaseGood <92409532+ricolxwz@users.noreply.github.com>
2023-09-26 06:59:20 +0800

    Update install.sh

--- Snell/install.sh
+++ Snell/install.sh
@@ -6,6 +6,55 @@ net.ipv6.conf.lo.disable_ipv6 = 0" >> /etc/sysctl.conf
 sysctl -p
 echo "---------- Prefer Ipv4 over Ipv6 ----------"
 echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
+echo "---------- git Configuration ----------"
+cd /root
+apt install git -y
+echo "---------- DDNS Configuration ----------"
+cd /root
+git clone https://github.com/timothymiller/cloudflare-ddns.git
+cd cloudflare-ddns
+touch config.json
+read -p "Enter api_token: " api
+read -p "Enter zone_id: " zone
+echo -e "{
+  \x22cloudflare\x22: [
+    {
+      \x22authentication\x22: {
+        \x22api_token\x22: \x22$api\x22
+      },
+      \x22zone_id\x22: \x22$zone\x22,
+      \x22subdomains\x22: [
+        {
+          \x22name\x22: \x22\x22,
+          \x22proxied\x22: false
+        }
+      ]
+    }
+  ],
+  \x22a\x22: true,
+  \x22aaaa\x22: true,
+  \x22purgeUnknownRecords\x22: false,
+  \x22ttl\x22: 300
+}" > config.json
+apt install python3 python3-venv -y
+python3 -m venv venv
+./start-sync.sh
+echo "---------- Crontab Configuration ----------"
+cd /var/spool/cron/crontabs
+touch root
+echo "*/15 * * * * cd /root/cloudflare-ddns && ./start-sync.sh" >> root
+echo "@reboot cd /root/cloudflare-ddns && ./start-sync.sh" >> root
+systemctl restart cron
+echo "---------- ACME Configuration ----------"
+cd /root
+curl https://get.acme.sh | sh
+read -p "Enter email: " email
+read -p "Enter domain: " domain
+read -p "Enter port: " port
+read -p "Enter wspath: " path
+~/.acme.sh/acme.sh --register-account -m $email
+~/.acme.sh/acme.sh --issue -d $domain --standalone
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
 echo "---------- Snell Configuration ----------"
 cd /root
 wget https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-amd64.zip
