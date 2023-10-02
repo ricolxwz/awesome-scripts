f9646273f9aec42845bf3cc3729d99a2fdf5702b
McflurryTaseGood <92409532+ricolxwz@users.noreply.github.com>
2023-10-02 17:09:32 +0800

    Update warp.sh

--- Common/warp.sh
+++ Common/warp.sh
@@ -1,7 +1,7 @@
 apt install curl -y
 curl https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
-apt update && apt install cloudflare-warp
+apt update && apt install cloudflare-warp -y
 warp-cli register
 warp-cli set-mode proxy
 warp-cli connect
