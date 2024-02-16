302dbd37b11ae4f3addfb7e18c5d7afedb10cfe9
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-02-16 20:17:15 +1100

    Create frp.sh

---
+++ Docker/frp.sh
@@ -0,0 +1,38 @@
+sudo apt update -y
+sudo apt install curl -y
+echo "net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.all.disable_ipv6 = 0
+net.ipv6.conf.lo.disable_ipv6 = 0" | sudo tee -a /etc/sysctl.conf > /dev/null
+sudo sysctl -p
+echo "precedence ::ffff:0:0/96  100" | sudo tee -a /etc/gai.conf > /dev/null
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+CONFIG='{
+  "ipv6": true,
+  "fixed-cidr-v6": "2001:db8:1::/64",
+  "experimental": true,
+  "ip6tables": true
+}'
+FILE="/etc/docker/daemon.json"
+if [ -f "$FILE" ]; then
+    sudo cp $FILE ${FILE}.bak
+    echo "$CONFIG" | sudo tee $FILE > /dev/null
+else
+    echo "$CONFIG" | sudo tee $FILE > /dev/null
+fi
+sudo systemctl restart docker
+echo -e "
+bindPort = shuf -i 1-65535 -n 1
+auth.token = \x22$(openssl rand -base64 20)\x22
+" | sudo tee /etc/frp/frps.toml > /dev/null
+sudo docker run --restart=always --network host -d -v /etc/frp/frps.toml:/etc/frp/frps.toml --name frps snowdreamtech/frps
+sudo grep "bindPort" /etc/frp/frps.toml | awk -F '= ' '{print $2}'
+sudo grep "auth.token" /etc/frp/frps.toml | awk -F '= ' '{print $2}' | tr -d '"'
+sudo reboot
