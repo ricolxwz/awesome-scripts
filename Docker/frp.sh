94ec214015a92c7733bd903cf46c57e5510e9b8c
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-02-16 21:10:23 +1100

    Update frp.sh

--- Docker/frp.sh
+++ Docker/frp.sh
@@ -30,16 +30,12 @@ fi
 sudo systemctl restart docker
 sudo mkdir -p /etc/frp
 echo -e "
+bindPort = $(shuf -i 1-65535 -n 1)
 quicBindPort = $(shuf -i 1-65535 -n 1)
 auth.token = \x22$(openssl rand -base64 20)\x22
-webServer.port = $(shuf -i 1-65535 -n 1)
-webServer.user = "admin"
-webServer.password = \x22$(openssl rand -base64 20)\x22
 " | sudo tee /etc/frp/frps.toml > /dev/null
 sudo docker run --restart=always --network host -d -v /etc/frp/frps.toml:/etc/frp/frps.toml --name frps snowdreamtech/frps
+echo "bindPort = $(sudo grep 'bindPort' /etc/frp/frps.toml | awk -F '= ' '{print $2}')"
 echo "quicBindPort = $(sudo grep 'quicBindPort' /etc/frp/frps.toml | awk -F '= ' '{print $2}')"
 echo "auth.token = $(sudo grep 'auth.token' /etc/frp/frps.toml | awk -F '= ' '{print $2}' | tr -d '\"')"
-echo "webServer.port = $(sudo grep 'webServer.port' /etc/frp/frps.toml | awk -F '= ' '{print $2}')"
-echo "webServer.user = $(sudo grep 'webServer.user' /etc/frp/frps.toml | awk -F '= ' '{print $2}' | tr -d '\"')"
-echo "webServer.password = $(sudo grep 'webServer.password' /etc/frp/frps.toml | awk -F '= ' '{print $2}' | tr -d '\"')"
 sudo reboot
