b9e90bbaf18386bb43a95cd874d776c7e306d1ed
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-02-18 10:39:45 +1100

    Update install.sh

--- Wireguard/install.sh
+++ Wireguard/install.sh
@@ -1,5 +1,5 @@
 apt update -y
-apt install wireguard -y
+apt install wireguard resolvconf -y
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
