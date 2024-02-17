691830b35dee1210f362c396c12b6d5188fee6f7
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-02-18 10:41:36 +1100

    Update install.sh

--- Wireguard/install.sh
+++ Wireguard/install.sh
@@ -26,4 +26,10 @@ PublicKey =  $(cat client1.key.pub)
 AllowedIPs = 10.0.8.0/24, fd86::0/112" > wg0.conf
 systemctl enable wg-quick@wg0
 wg-quick up wg0
-cat server.key && cat server.key.pub && cat client1.key && cat client1.key.pub
+echo "server.key:" && cat server.key && \
+echo "" && \
+echo "server.key.pub:" && cat server.key.pub && \
+echo "" && \
+echo "client1.key:" && cat client1.key && \
+echo "" && \
+echo "client1.key.pub:" && cat client1.key.pub
