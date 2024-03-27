7c87dd75ebdc1b4a32b4d722d791da60cdf57f5e
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-03-28 00:16:42 +1100

    Create setup.sh

---
+++ Alist/setup.sh
@@ -0,0 +1,40 @@
+apt update -y
+echo "---------- DDNS Configuration ----------"
+cd /root
+git clone https://github.com/timothymiller/cloudflare-ddns.git
+cd cloudflare-ddns
+touch config.json
+read -p "Enter api_token: " api
+read -p "Enter zone_id: " zone
+echo -e "{
+  \x22cloudflare\x22: [
+    {
+      \x22authentication\x22: {
+        \x22api_token\x22: \x22$api\x22
+      },
+      \x22zone_id\x22: \x22$zone\x22,
+      \x22subdomains\x22: [
+        {
+          \x22name\x22: \x22\x22,
+          \x22proxied\x22: false
+        }
+      ]
+    }
+  ],
+  \x22a\x22: true,
+  \x22aaaa\x22: true,
+  \x22purgeUnknownRecords\x22: false,
+  \x22ttl\x22: 300
+}" > config.json
+apt install python3 python3-venv -y
+python3 -m venv venv
+./start-sync.sh
+echo "---------- Crontab Configuration ----------"
+cd /var/spool/cron/crontabs
+touch root
+echo "*/15 * * * * cd /root/cloudflare-ddns && ./start-sync.sh" >> root
+echo "@reboot cd /root/cloudflare-ddns && ./start-sync.sh" >> root
+systemctl restart cron
+curl -fsSL "https://alist.nn.ci/v3.sh" | bash -s install
+cd /opt/alist
+./alist admin random
