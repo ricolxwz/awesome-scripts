07dd2f5084c8d189ebb32cf0219c6155910c8dc8
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-03-28 00:38:44 +1100

    Update setup.sh

--- Alist/setup.sh
+++ Alist/setup.sh
@@ -35,6 +35,56 @@ touch root
 echo "*/15 * * * * cd /root/cloudflare-ddns && ./start-sync.sh" >> root
 echo "@reboot cd /root/cloudflare-ddns && ./start-sync.sh" >> root
 systemctl restart cron
+echo "---------- ACME Configuration ----------"
+cd /root
+curl https://get.acme.sh | sh
+read -p "Enter email: " email
+read -p "Enter domain: " domain
+echo "Please choose an SSL provider:"
+options=("letsencrypt" "buypass" "zerossl" "ssl.com" "google")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "letsencrypt")
+            echo "Switching to Let's Encrypt"
+            ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+            break
+            ;;  
+        "buypass")
+            echo "Switching to Buypass"
+            ~/.acme.sh/acme.sh --set-default-ca --server buypass
+            break
+            ;;
+        "zerossl")
+            echo "Switching to ZeroSSL"
+            ~/.acme.sh/acme.sh --set-default-ca --server zerossl
+            break
+            ;;
+        "ssl.com")
+            echo "Switching to SSL.com"
+            ~/.acme.sh/acme.sh --set-default-ca --server ssl.com
+            break
+            ;;
+        "google")
+            echo "Switching to Google Public CA"
+            ~/.acme.sh/acme.sh --set-default-ca --server google
+            break
+            ;;
+        *) echo "Invalid option. Please try again.";;
+    esac
+done
+echo "You have selected: $opt"
+~/.acme.sh/acme.sh --register-account -m $email
+~/.acme.sh/acme.sh --issue -d $domain --standalone
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
+echo "---------- Alist Configuration ----------"
 curl -fsSL "https://alist.nn.ci/v3.sh" | bash -s install
 cd /opt/alist
 ./alist admin random
+read -p "Enter https port: " port
+config_file="/opt/alist/data/config.json"
+sed -i "s/\"https_port\": [^,]*/\"https_port\": $port/" $config_file
+sed -i 's/"force_https": false/"force_https": true/' $config_file
+sed -i 's|"key_file": "[^"]*"|"key_file": "/root/private.key"|' $config_file
+sed -i 's|"cert_file": "[^"]*"|"cert_file": "/root/cert.crt"|' $config_file
+systemctl restart alist
