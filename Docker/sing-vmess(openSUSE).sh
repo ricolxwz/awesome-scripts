5e9c599030a05ae5a39cf2dd95a6ff35db7c4c45
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-04-25 23:18:21 +1000

    Update sing-vmess(openSUSE).sh

--- Docker/sing-vmess(openSUSE).sh
+++ Docker/sing-vmess(openSUSE).sh
@@ -6,7 +6,7 @@ sysctl -p
 echo "---------- IPv4优先级更高 ----------"
 echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
 echo "---------- 安装常用依赖 ----------"
-zypper install ca-certificates git cronie socat -y
+zypper --non-interactive install ca-certificates git cronie socat -y
 echo "---------- DDNS安装配置 ----------"
 cd /root
 git clone https://github.com/timothymiller/cloudflare-ddns.git
