29bcc0e1315a9d15863c9a127c455a8c94c45f80
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-04-26 21:47:33 +1000

    Update sing-vmess.sh

--- OpenSUSE/sing-vmess.sh
+++ OpenSUSE/sing-vmess.sh
@@ -158,6 +158,63 @@ set -e -o pipefail
 systemctl daemon-reload
 systemctl enable sing-box
 systemctl start sing-box
+echo "---------- Nginx Configuration ----------"
+cd /root
+zypper --non-interactive install nginx
+echo -e "user nobody;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enakbled/*.conf;
+events {
+        worker_connections 768;
+        # multi_accept on;
+}
+http {
+server {
+        listen 443 ssl;
+        listen [::]:443 ssl;
+        server_name $domain;
+        ssl_certificate       /root/cert.crt;
+        ssl_certificate_key   /root/private.key;
+        ssl_session_timeout 1d;
+        ssl_session_cache shared:MozSSL:10m;
+        ssl_session_tickets off;
+        ssl_protocols         TLSv1.2 TLSv1.3;
+        ssl_ciphers           ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+        ssl_prefer_server_ciphers off;
+        location / {
+                proxy_pass https://www.bing.com;
+                proxy_ssl_server_name on;
+                proxy_redirect off;
+                sub_filter_once off;
+                sub_filter \x22www.bing.com\x22 \x24server_name;
+                proxy_set_header Host \x22www.bing.com\x22;
+                proxy_set_header Referer \x24http_referer;
+                proxy_set_header X-Real-IP \x24remote_addr;
+                proxy_set_header User-Agent \x24http_user_agent;
+                proxy_set_header X-Forwarded-For \x24proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto https;
+                proxy_set_header Accept-Encoding \x22\x22;
+                proxy_set_header Accept-Language \x22zh-CN\x22;
+        }
+        location $path {
+                proxy_redirect off;
+                proxy_pass http://127.0.0.1:$port; 
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade \x24http_upgrade;
+                proxy_set_header Connection \x22upgrade\x22;
+                proxy_set_header Host \x24host;
+                proxy_set_header X-Real-IP \x24remote_addr;
+                proxy_set_header X-Forwarded-For \x24proxy_add_x_forwarded_for;
+        }
+}
+server {
+        listen 80;
+        server_name $domain;
+        rewrite ^(.*)\x24 https://\x24{server_name}\x241 permanent;
+}
+}" > /etc/nginx/nginx.conf
+systemctl restart nginx
 echo "---------- 密钥 ----------"
 cd /usr/local/etc/sing-box
 cat config.json | sed 's/,/\n/g' | grep "uuid" | sed 's/:/\n/g' | sed '1d' | sed 's/}//g' | sed 's/"//g' | tr -d ' '
