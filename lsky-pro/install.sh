5455460c53240cd1f82884d0cbf26b3233b15c0c
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-05-20 07:46:21 +1000

    Create install.sh

---
+++ lsky-pro/install.sh
@@ -0,0 +1,30 @@
+echo "---------- 安装基础依赖 ----------"
+apt install cron git curl wget unzip -y
+echo "---------- 安装php ----------"
+apt install php -y
+echo "---------- 安装mysql ----------"
+apt install gnupg -y
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.30-1_all.deb
+dpkg -i mysql-apt-config*
+apt update
+apt install mysql-server
+echo "---------- 安装nginx ----------"
+apt install php-fpm -y
+systemctl stop apache2
+systemctl disable apache2
+apt purge apache2 -y
+apt autoremove -y
+apt install nginx -y
+echo "---------- 安装redis ----------"
+apt install redis -y
+echo "---------- 安装php扩展 ----------"
+apt install php-mysqli php-curl php-mbstring php-imagick php-redis -y
+systemctl restart nginx
+echo "---------- 建立网站 ----------"
+read -p "Please input download url: " download_url
+cd /var/www/html
+rm -rf * .*
+wget $download_url
+unzip *
+ls -a | grep 'zip' | xargs -d '\n' rm
+systemctl reload nginx
