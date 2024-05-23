1e43f517eb20958cca494571fc08de654d410b35
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-05-24 00:29:18 +1000

    Create ssl.sh

---
+++ wordpress/ssl.sh
@@ -0,0 +1,45 @@
+apt update -y
+apt install git socat ca-certificates -y
+echo "---------- ACME Configuration ----------"
+cd /root
+curl https://get.acme.sh | sh
+read -p "Enter email: " email
+read -p "Enter domain: " domain
+read -p "Please input where the cert should be put (path): " cert_path
+echo "Please choose an SSL provider:"
+options=("letsencrypt" "buypass" "zerossl" "ssl.com" "google")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "letsencrypt")
+            echo "Switching to Let's Encrypt"
+            ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+            break
+            ;;  
+        "buypass")
+            echo "Switching to Buypass"
+            ~/.acme.sh/acme.sh --set-default-ca --server buypass
+            break
+            ;;
+        "zerossl")
+            echo "Switching to ZeroSSL"
+            ~/.acme.sh/acme.sh --set-default-ca --server zerossl
+            break
+            ;;
+        "ssl.com")
+            echo "Switching to SSL.com"
+            ~/.acme.sh/acme.sh --set-default-ca --server ssl.com
+            break
+            ;;
+        "google")
+            echo "Switching to Google Public CA"
+            ~/.acme.sh/acme.sh --set-default-ca --server google
+            break
+            ;;
+        *) echo "Invalid option. Please try again.";;
+    esac
+done
+echo "You have selected: $opt"
+~/.acme.sh/acme.sh --register-account -m $email
+~/.acme.sh/acme.sh --issue -d $domain --standalone
+~/.acme.sh/acme.sh --installcert -d $domain --key-file $cert_path/fullchain.pem / --fullchain-file $cert_path/privkey.pem
