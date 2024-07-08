b03bfdf26cc09625d0faf3f5d1f0be3379f94a6a
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-08 19:59:58 +1000

    Create pyenv.sh

---
+++ Linux/pyenv.sh
@@ -0,0 +1,13 @@
+cd ~
+sudo apt install git curl -y
+curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source .bashrc
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+pyenv install 3.12
+pyenv global 3.12
+pip install poetry
