26e70425a7dd56679585bd97c4c6d7c305255c4b
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-08 21:59:30 +0800

    Update pyenv.sh

--- Linux/pyenv.sh
+++ Linux/pyenv.sh
@@ -7,7 +7,7 @@ echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source .bashrc
 sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 pyenv install 3.12
 pyenv global 3.12
 pip install poetry
