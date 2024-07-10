8be77180e0ceca58db65b5c9d551bb94e5be1b9c
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-10 20:37:38 +1000

    Create network.sh

---
+++ Linux/network.sh
@@ -0,0 +1,20 @@
+cd /etc/netplan
+sudo rm -rf 50*
+echo "
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    $interface:
+      dhcp4: false
+      dhcp6: false
+      addresses:
+        - 192.168.91.100/24
+      routes:
+        - to: default
+          via: 192.168.91.2
+      nameservers:
+        addresses: [192.168.91.2]" | sudo tee /etc/netplan/01-netcfg.yaml > /dev/null
+sudo chmod 600 01-netcfg.yaml
+sudo netplan generate
+sudo netplan apply
