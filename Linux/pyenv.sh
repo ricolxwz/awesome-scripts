35011d240bcec855363ca192d2459d42f95001e4
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-10 20:59:07 +0800

    Update pyenv.sh

--- Linux/pyenv.sh
+++ Linux/pyenv.sh
@@ -2,7 +2,7 @@ cd ~
 sudo apt install git curl -y
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source .bashrc
 sudo apt install build-essential libssl-dev zlib1g-dev \
