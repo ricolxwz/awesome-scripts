680eceb0e270110ef99f907ff81005701addbc38
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-10 20:51:42 +0800

    Update setup.sh

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -1,15 +1,12 @@
 sudo apt update -y
 sudo apt install wget
 cd ~
-wget -O input.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/input.sh"
 wget -O user.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/user.sh"
 wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/network.sh"
 wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/packages.sh"
 wget -O docker.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/docker.sh"
 wget -O pyenv.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/pyenv.sh"
 chmod a+x *.sh
-ip a
-./input.sh
 ./user.sh
 ./packages.sh
 ./docker.sh
