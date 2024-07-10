086e9a28565936b662d18f8d7d110c3bfa0b8c3e
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-10 21:52:13 +1000

    Update user.sh

--- Linux/user.sh
+++ Linux/user.sh
@@ -1,4 +1,4 @@
 cd /etc
 sudo chmod 640 sudoers
-echo '${USER} ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 sudo chmod 440 sudoers
