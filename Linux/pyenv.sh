ee70ceccdb98e8724640b9e6dc00435cc1ac1244
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-10 21:03:11 +0800

    Update pyenv.sh

--- Linux/pyenv.sh
+++ Linux/pyenv.sh
@@ -4,7 +4,7 @@ curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-source .bashrc
+source ~/.bashrc
 sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
