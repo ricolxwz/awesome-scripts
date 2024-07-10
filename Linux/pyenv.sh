52705630c1ed241526a50c06fa66d4e2c598b120
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-10 22:05:14 +1000

    Update pyenv.sh

--- Linux/pyenv.sh
+++ Linux/pyenv.sh
@@ -5,7 +5,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source .bashrc
-sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 pyenv install 3.12
