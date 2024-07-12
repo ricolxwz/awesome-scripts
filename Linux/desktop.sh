2a51d93c703781e173eeee9efcfd8426ea10a8be
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-12 16:13:06 +1000

    Update desktop.sh

--- Linux/desktop.sh
+++ Linux/desktop.sh
@@ -1,53 +1,58 @@
-cd ~
-sudo apt install unzip wget -y
-sudo add-apt-repository universe
-sudo apt install \
-  gnome-tweak-tool \
-  gnome-shell-extension-manager \
-  gnome-software \
-  ibus-rime -y
-wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/config.tar.gz"
-wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/opencc.tar.gz"
-wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict1.tar.gz"
-wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict2.tar.gz"
-wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict3.tar.gz"
-wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict4.tar.gz"
-wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict5.tar.gz"
-tar -xzvf config.tar.gz config
-tar -xzvf opencc.tar.gz opencc
-tar -xzvf dict1.tar.gz dict1
-tar -xzvf dict2.tar.gz dict2
-tar -xzvf dict3.tar.gz dict3
-tar -xzvf dict4.tar.gz dict4
-tar -xzvf dict5.tar.gz dict5
-rm *.tar.gz
-mkdir ~/dicts
-mv dict1/* ~/dicts/
-mv dict2/* ~/dicts/
-mv dict3/* ~/dicts/
-mv dict4/* ~/dicts/
-mv dict5/* ~/dicts/
-mkdir -p ~/.config/ibus/rime
-mv ~/dicts ~/.config/ibus/rime/
-mv ~/opencc ~/.config/ibus/rime/
-mv ~/config/* ~/.config/ibus/rime/
-rm -rf ~/config
-rm -rf ~/opencc
-rm -rf ~/dicts
-rm -rf dict*
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Iosevka.zip"
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IosevkaTerm.zip"
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/UbuntuMono.zip"
-unzip Iosevka.zip -d Iosevka
-unzip IosevkaTerm.zip -d IosevkaTerm
-unzip UbuntuMono.zip -d UbuntuMono
-mkdir -p ~/.local/share/fonts
-mv Iosevka ~/.local/share/fonts/
-mv IosevkaTerm ~/.local/share/fonts/
-mv UbuntuMono ~/.local/share/fonts/
-rm Iosevka.zip
-rm IosevkaTerm.zip
-rm UbuntuMono.zip
-fc-cache -v
-wget -O code.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-sudo dpkg -i code.deb
+read -p "是否需要安装桌面环境和相关软件？(y/n): " answer
+if [ "$answer" = "y" ]; then
+    cd ~
+    sudo apt install unzip wget -y
+    sudo add-apt-repository universe
+    sudo apt install \
+      gnome-tweak-tool \
+      gnome-shell-extension-manager \
+      gnome-software \
+      ibus-rime -y
+    wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/config.tar.gz"
+    wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/opencc.tar.gz"
+    wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict1.tar.gz"
+    wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict2.tar.gz"
+    wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict3.tar.gz"
+    wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict4.tar.gz"
+    wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict5.tar.gz"
+    tar -xzvf config.tar.gz config
+    tar -xzvf opencc.tar.gz opencc
+    tar -xzvf dict1.tar.gz dict1
+    tar -xzvf dict2.tar.gz dict2
+    tar -xzvf dict3.tar.gz dict3
+    tar -xzvf dict4.tar.gz dict4
+    tar -xzvf dict5.tar.gz dict5
+    rm *.tar.gz
+    mkdir ~/dicts
+    mv dict1/* ~/dicts/
+    mv dict2/* ~/dicts/
+    mv dict3/* ~/dicts/
+    mv dict4/* ~/dicts/
+    mv dict5/* ~/dicts/
+    mkdir -p ~/.config/ibus/rime
+    mv ~/dicts ~/.config/ibus/rime/
+    mv ~/opencc ~/.config/ibus/rime/
+    mv ~/config/* ~/.config/ibus/rime/
+    rm -rf ~/config
+    rm -rf ~/opencc
+    rm -rf ~/dicts
+    rm -rf dict*
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Iosevka.zip"
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IosevkaTerm.zip"
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/UbuntuMono.zip"
+    unzip Iosevka.zip -d Iosevka
+    unzip IosevkaTerm.zip -d IosevkaTerm
+    unzip UbuntuMono.zip -d UbuntuMono
+    mkdir -p ~/.local/share/fonts
+    mv Iosevka ~/.local/share/fonts/
+    mv IosevkaTerm ~/.local/share/fonts/
+    mv UbuntuMono ~/.local/share/fonts/
+    rm Iosevka.zip
+    rm IosevkaTerm.zip
+    rm UbuntuMono.zip
+    fc-cache -v
+    wget -O code.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+    sudo dpkg -i code.deb
+else
+    echo "未执行任何操作."
+fi
