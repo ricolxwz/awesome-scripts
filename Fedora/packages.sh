ee21ffa90db1a7b42a1dcf74b700d04d5787a77a
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-12 21:48:33 +1000

    Create packages.sh

---
+++ Fedora/packages.sh
@@ -0,0 +1,44 @@
+sudo dnf update -y
+sudo dnf install -y \
+  tree \
+  git \
+  neovim \
+  tldr \
+  cargo \
+  ranger \
+  zoxide \
+  fzf \
+  axel \
+  plocate \
+  trash-cli \
+  duf \
+  gh \
+  bat \
+  htop \
+  httping \
+  aria2 \
+  tmux \
+  gping \
+  speedtest-cli \
+  httpie \
+  fuse3
+
+cargo install \
+  du-dust \
+  eza \
+  procs \
+  ripgrep
+
+echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
+echo 'export PATH="$PATH:/home/wenzexu/.cargo/bin"' >> ~/.bashrc
+echo 'alias bat="batcat"' >> ~/.bashrc
+echo 'alias vi="nvim"' >> ~/.bashrc
+echo 'alias vim="nvim"' >> ~/.bashrc
+echo 'alias rm="trash-put"' >> ~/.bashrc
+echo 'alias ll="eza -al -s=time"' >> ~/.bashrc
+echo 'alias cd="z"' >> ~/.bashrc
+
+source ~/.bashrc
+
+git config --global user.name "wenzexu"
+git config --global user.email "ricol.xwz@outlook.com"
