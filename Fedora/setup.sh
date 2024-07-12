0f87005311c104cd85c0bbc2b9e827597435b873
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-12 22:46:09 +1000

    Update setup.sh

--- Fedora/setup.sh
+++ Fedora/setup.sh
@@ -1,1 +1,18 @@
-
+sudo dnf update -y
+sudo dnf install wget -y
+cd ~
+wget -O user.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/user.sh"
+wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/network.sh"
+wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/packages.sh"
+wget -O docker.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/docker.sh"
+wget -O pyenv.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/pyenv.sh"
+wget -O desktop.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/desktop.sh"
+chmod a+x *.sh
+./user.sh
+./packages.sh
+./docker.sh
+./pyenv.sh
+./desktop.sh
+./network.sh
+cd ~
+rm -rf *.sh
