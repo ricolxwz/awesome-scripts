405e8745b84721d948db326200159bb72649277f
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-12 21:50:33 +1000

    Create pyenv.sh

---
+++ Fedora/pyenv.sh
@@ -0,0 +1,30 @@
+cd ~
+sudo dnf install git curl -y
+
+read -p "请输入要安装的Python版本号(以空格分隔): " pyv
+read -p "请输入使用的全局Python版本号: " pyvm
+
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+source ~/.bashrc
+source ~/.profile
+
+dnf install make gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2 -y
+
+pyenv install $pyv
+pyenv global $pyvm
+
+pip install poetry
+
+source ~/.bashrc
+source ~/.profile
