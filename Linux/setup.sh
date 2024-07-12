141b167aa08e425305e3e89cd0b14107a3d6698a
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-12 16:10:38 +1000

    Update setup.sh

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -6,11 +6,13 @@ wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/m
 wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/packages.sh"
 wget -O docker.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/docker.sh"
 wget -O pyenv.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/pyenv.sh"
+wget -O desktop.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Linux/desktop.sh"
 chmod a+x *.sh
 ./user.sh
 ./packages.sh
 ./docker.sh
 ./pyenv.sh
 ./network.sh
+./desktop.sh
 cd ~
 rm -rf *.sh
