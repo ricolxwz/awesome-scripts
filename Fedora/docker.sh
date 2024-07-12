1d327fd75ac413d815fc49349c811b74c53e3e38
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-12 21:44:27 +1000

    Create docker.sh

---
+++ Fedora/docker.sh
@@ -0,0 +1,6 @@
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
