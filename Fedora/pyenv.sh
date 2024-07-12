9f2da99a1f5856b22feeb7b67747fce3b9901a1b
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-12 22:59:56 +1000

    Update pyenv.sh

--- Fedora/pyenv.sh
+++ Fedora/pyenv.sh
@@ -8,12 +8,10 @@ curl https://pyenv.run | bash
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-echo 'eval "$(pyenv init --path)"' >> ~/.profile
 echo 'eval "$(pyenv init -)"' >> ~/.profile
 
 source ~/.bashrc
