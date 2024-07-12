ba84fa0f84f0f432093dd045cbd67c7fd934ceef
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-12 15:54:37 +1000

    Update desktop.sh

--- Linux/desktop.sh
+++ Linux/desktop.sh
@@ -6,11 +6,15 @@ sudo apt install \
   gnome-shell-extension-manager \
   gnome-software \
   ibus-rime -y
+wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/config.tar.gz"
+wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/opencc.tar.gz"
 wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict1.tar.gz"
 wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict2.tar.gz"
 wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict3.tar.gz"
 wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict4.tar.gz"
 wget "https://github.com/ricolxwz/awesome-scripts/raw/master/Linux/rime/dict5.tar.gz"
+tar -xzvf config.tar.gz config
+tar -xzvf opencc.tar.gz opencc
 tar -xzvf dict1.tar.gz dict1
 tar -xzvf dict2.tar.gz dict2
 tar -xzvf dict3.tar.gz dict3
