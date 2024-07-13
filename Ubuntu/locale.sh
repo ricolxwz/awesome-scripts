b8eb641364423845ea83b4fd6abbb5fd04f97575
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-13 21:38:07 +1000

    Create locale.sh

---
+++ Ubuntu/locale.sh
@@ -0,0 +1,16 @@
+sudo locale-gen zh_CN.UTF-8 en_US.UTF-8
+echo 'export LANG=zh_CN.UTF-8' >> ~/.bashrc
+echo 'export LANGUAGE=' >> ~/.bashrc
+echo 'export LC_CTYPE="en_US.UTF-8"' >> ~/.bashrc
+echo 'export LC_NUMERIC=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_TIME=zh_CN.UTF-8' >> ~/.bashrc
+echo 'export LC_COLLATE="en_US.UTF-8"' >> ~/.bashrc
+echo 'export LC_MONETARY=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_MESSAGES="en_US.UTF-8"' >> ~/.bashrc
+echo 'export LC_PAPER=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_NAME=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_ADDRESS=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_TELEPHONE=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_MEASUREMENT=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_IDENTIFICATION=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_ALL=' >> ~/.bashrc
