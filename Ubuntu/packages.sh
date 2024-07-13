b5119553a9a3da0cc8cc45dc6af83a3fe358bd5c
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-13 21:37:41 +1000

    Update packages.sh

--- Ubuntu/packages.sh
+++ Ubuntu/packages.sh
@@ -35,22 +35,7 @@ echo 'alias vim="nvim"' >> ~/.bashrc
 echo 'alias rm="trash-put"' >> ~/.bashrc
 echo 'alias ll="eza -al -s=time"' >> ~/.bashrc
 echo 'alias cd="z"' >> ~/.bashrc
-sudo locale-gen zh_CN.UTF-8 en_US.UTF-8
-echo 'export LANG=zh_CN.UTF-8' >> ~/.bashrc
-echo 'export LANGUAGE=' >> ~/.bashrc
-echo 'export LC_CTYPE="en_US.UTF-8"' >> ~/.bashrc
-echo 'export LC_NUMERIC=en_US.UTF-8' >> ~/.bashrc
-echo 'export LC_TIME=zh_CN.UTF-8' >> ~/.bashrc
-echo 'export LC_COLLATE="en_US.UTF-8"' >> ~/.bashrc
-echo 'export LC_MONETARY=en_US.UTF-8' >> ~/.bashrc
-echo 'export LC_MESSAGES="en_US.UTF-8"' >> ~/.bashrc
-echo 'export LC_PAPER=en_US.UTF-8' >> ~/.bashrc
-echo 'export LC_NAME=en_US.UTF-8' >> ~/.bashrc
-echo 'export LC_ADDRESS=en_US.UTF-8' >> ~/.bashrc
-echo 'export LC_TELEPHONE=en_US.UTF-8' >> ~/.bashrc
-echo 'export LC_MEASUREMENT=en_US.UTF-8' >> ~/.bashrc
-echo 'export LC_IDENTIFICATION=en_US.UTF-8' >> ~/.bashrc
-echo 'export LC_ALL=' >> ~/.bashrc
+source ~/.bashrc
 git config --global user.name "wenzexu"
 git config --global user.email "ricol.xwz@outlook.com"
 token=""
