682e142fdd681c132a8601cb1dbdcfa3c5573241
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-15 10:45:34 +1000

    Update sing-box.sh

--- Fedora/sing-box.sh
+++ Fedora/sing-box.sh
@@ -1,3 +1,5 @@
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://sing-box.app/sing-box.repo
 sudo dnf install sing-box -y
+sudo systemctl enable sing-box
+sudo systemctl restart sing-box
