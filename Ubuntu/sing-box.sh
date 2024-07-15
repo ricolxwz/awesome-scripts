0bb4713ce376cef4766c5c6a5d4f0a31abcdb865
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-15 10:30:42 +1000

    Update sing-box.sh

--- Ubuntu/sing-box.sh
+++ Ubuntu/sing-box.sh
@@ -5,14 +5,6 @@ sudo chmod a+r /etc/apt/keyrings/sagernet.asc
 echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | \
   sudo tee /etc/apt/sources.list.d/sagernet.list > /dev/null
 sudo apt-get update
-sudo apt-get install sing-box # or sing-box-beta
-while true; do
-    read -n 1 -p "请将config.json文件放到用户目录下, 放好请输入y: " key
-    if [[ $key == "y" ]]; then
-        break
-    fi
-done
-mkdir -p /etc/sing-box
-sudo mv config.json /etc/sing-box/
+sudo apt-get install sing-box -y
 sudo systemctl enable sing-box
 sudo systemctl restart sing-box
