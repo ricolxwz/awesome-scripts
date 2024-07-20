7c4bd0a76c42b9c3a9a1b5ad6b962d130dd502a3
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-20 11:18:44 +1000

    Update setup.sh

--- Fedora/setup.sh
+++ Fedora/setup.sh
@@ -5,7 +5,7 @@ wget -O user.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/mast
 wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/network.sh"
 wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/packages.sh"
 wget -O docker.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/docker.sh"
-wget -O pyenv.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/pyenv.sh"
+# wget -O pyenv.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/pyenv.sh"
 wget -O sing-box.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/sing-box.sh"
 wget -O desktop.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/desktop.sh"
 wget -O locale.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/Fedora/locale.sh"
@@ -13,7 +13,7 @@ chmod a+x *.sh
 ./user.sh
 ./packages.sh
 ./docker.sh
-./pyenv.sh
+# ./pyenv.sh
 ./desktop.sh
 ./sing-box.sh
 ./network.sh
