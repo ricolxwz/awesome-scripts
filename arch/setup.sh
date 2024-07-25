91a0e07b8651ce04b76acf45d02b459e5ed8a80a
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-26 09:59:47 +1000

    Update setup.sh

--- arch/setup.sh
+++ arch/setup.sh
@@ -4,6 +4,7 @@ cd ~
 wget -O user.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/user.sh"
 wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/network.sh"
 wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/packages.sh"
+wget -O docker.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/docker.sh"
 wget -O sing-box.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/sing-box.sh"
 wget -O desktop.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/desktop.sh"
 wget -O locale.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/locale.sh"
@@ -11,6 +12,7 @@ wget -O vm.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master
 chmod a+x *.sh
 ./user.sh
 ./packages.sh
+./docker.sh
 ./desktop.sh
 ./network.sh
 ./locale.sh
