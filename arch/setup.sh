86bbcd3e048c971e90cf5d0ae6efef7ee6f15de5
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-26 09:51:19 +1000

    Create setup.sh

---
+++ arch/setup.sh
@@ -0,0 +1,18 @@
+sudo pacman -Syu --noconfirm
+sudo pacman -S wget --needed --noconfirm
+cd ~
+wget -O user.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/user.sh"
+wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/network.sh"
+wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/packages.sh"
+wget -O sing-box.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/sing-box.sh"
+wget -O desktop.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/desktop.sh"
+wget -O locale.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/locale.sh"
+wget -O vm.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/vm.sh"
+chmod a+x *.sh
+./user.sh
+./packages.sh
+./desktop.sh
+./network.sh
+./locale.sh
+cd ~
+rm -rf *.sh
