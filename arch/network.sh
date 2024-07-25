0a607866c5ddfec4d04f9c1e28056c65a0cb5887
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-26 01:17:14 +1000

    Create network.sh

---
+++ arch/network.sh
@@ -0,0 +1,75 @@
+DEFAULT_IP=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d'/' -f1 | head -n 1)
+DEFAULT_GATEWAY=$(ip route | grep default | awk '{print $3}')
+DEFAULT_DNS=$(ip route | grep default | awk '{print $3}')
+
+echo '
+alias proxy="
+    export http_proxy=http://127.0.0.1:5353;
+    export https_proxy=http://127.0.0.1:5353;
+    export all_proxy=http://127.0.0.1:5353;
+    export no_proxy=http://127.0.0.1:5353;
+    export HTTP_PROXY=http://127.0.0.1:5353;
+    export HTTPS_PROXY=http://127.0.0.1:5353;
+    export ALL_PROXY=http://127.0.0.1:5353;
+    export NO_PROXY=http://127.0.0.1:5353;"
+alias unproxy="
+    unset http_proxy;
+    unset https_proxy;
+    unset all_proxy;
+    unset no_proxy;
+    unset HTTP_PROXY;
+    unset HTTPS_PROXY;
+    unset ALL_PROXY;
+    unset NO_PROXY"
+' >> ~/.bashrc
+source ~/.bashrc
+
+ip r
+ip a
+
+sudo systemctl disable dhcpcd
+sudo systemctl stop dhcpcd
+sudo systemctl enable systemd-networkd
+sudo systemctl start systemd-networkd
+
+read -p "请输入公钥: " key
+interface=$(ip -o -4 route show to default | awk '{print $5}')
+read -p "请输入静态IP地址 [默认: $DEFAULT_IP]: " ip
+ip=${ip:-$DEFAULT_IP}
+read -p "请输入网关IP地址 [默认: $DEFAULT_GATEWAY]: " gateway
+gateway=${gateway:-$DEFAULT_GATEWAY}
+read -p "请输入DNS地址 [默认: $DEFAULT_DNS]: " dns
+dns=${dns:-$DEFAULT_DNS}
+
+sudo dnf install -y openssh-clients openssh-server
+sudo systemctl enable sshd
+sudo systemctl start sshd
+
+mkdir -p ~/.ssh
+echo "$key" > ~/.ssh/authorized_keys
+
+echo "请输入用于Github的SSH私钥(请使用ed25519加密, 输入完成后换行输入EOF回车):"
+ssh_private_key=""
+while IFS= read -r line; do
+    if [ "$line" == "EOF" ]; then
+        break
+    fi
+    ssh_private_key+="$line"$'\n'
+done
+if [ -n "$ssh_private_key" ]; then
+    echo "$ssh_private_key" > ~/.ssh/id_ed25519
+    chmod 600 ~/.ssh/id_ed25519
+else
+    echo "未输入SSH私钥, 跳过保存过程"
+fi
+
+echo "[Match]
+Name=${interface}
+[Network]
+Address=${ip}/24
+Gateway=${gateway}
+DNS=${dns}" | sudo tee /etc/systemd/network/99-vm.network > /dev/null
+
+echo "Bye Bye~, 请尝试用新的IP访问此机器"
+
+sudo systemctl restart sshd
