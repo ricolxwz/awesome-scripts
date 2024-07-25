252662f484cf27673fffe430d1e6b816bad808ee
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-25 22:54:24 +1000

    Update desktop.sh

--- Ubuntu/desktop.sh
+++ Ubuntu/desktop.sh
@@ -83,8 +83,7 @@ if [ "$answer" = "y" ]; then
     if [ "$im_version" = "fcitx5" ]; then
         sudo apt install -y \
             fcitx5 \
-            fcitx5-rime \
-            fcitx5-material-color
+            fcitx5-rime
         echo "export GTK_IM_MODULE=fcitx5" >> ~/.bashrc
         echo "export XMODIFIERS=@im=fcitx5" >> ~/.bashrc
         echo "export QT_IM_MODULE=fcitx5" >> ~/.bashrc
@@ -94,9 +93,6 @@ if [ "$answer" = "y" ]; then
         mv ~/opencc ~/.local/share/fcitx5/rime/
         mv ~/config/* ~/.local/share/fcitx5/rime/
         rm ~/.local/share/fcitx5/rime/ibus_rime.custom.yaml
-        echo "Vertical Candidate List=False
-        PerScreenDPI=True
-        Theme=Material-Color-Black" > ~/.config/fcitx5/conf/classicui.conf
     fi
     rm -rf ~/config
     rm -rf ~/opencc
