6b61a6741498517050337dd11d0e1e2aa8a6d1d3
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-26 11:08:52 +1000

    Update setup.sh

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,7 +1,6 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -S wget --needed --noconfirm
 cd ~
-wget -O user.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/user.sh"
 wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/network.sh"
 wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/packages.sh"
 wget -O docker.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/docker.sh"
@@ -10,7 +9,6 @@ wget -O desktop.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/m
 wget -O locale.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/locale.sh"
 wget -O conda.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/conda.sh"
 chmod a+x *.sh
-./user.sh
 ./packages.sh
 ./docker.sh
 ./conda.sh
