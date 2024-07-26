a155735d61d3b263ae41d417a82e4eb07a9dde15
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-26 11:08:26 +1000

    Delete arch/vm.sh

--- arch/vm.sh
+++
@@ -1,6 +0,0 @@
-sudo pacman -S open-vm-tools
-sudo systemctl enable vmtoolsd
-sudo systemctl start vmtoolsd
-sudo systemctl enable vmware-vmblock-fuse
-sudo systemctl start vmware-vmblock-fuse
-sudo pacman -S gtkmm3
