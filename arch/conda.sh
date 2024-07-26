aa1cafbca44c429f97e9b85efe3aff1c21874066
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-26 10:05:30 +1000

    Create conda.sh

---
+++ arch/conda.sh
@@ -0,0 +1,9 @@
+read -p "是否为ARM架构？(y/n, 留空或其他为amd64架构): " arm_answer
+if [ "$arm_answer" = "y" ]; then
+    wget -O miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh"
+else
+    wget -O miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
+fi
+chmod a+x miniforge3.sh
+./miniforge3.sh
+rm miniforge3.sh
