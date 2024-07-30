80ef1bb1d0b0dd83a20282feeb382f5c78ed04a5
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-07-30 23:57:45 +1000

    Create alist.sh

---
+++ arch/alist.sh
@@ -0,0 +1,2 @@
+docker run -d --restart=always -v /etc/alist:/opt/alist/data -p 5244:5244 -e PUID=0 -e PGID=0 -e UMASK=022 --name="alist" xhofe/alist:latest
+docker exec -it alist ./alist admin set password
