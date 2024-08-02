cec070088f39f849f4933a88bddc6db56432cf1b
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-08-02 21:55:33 +1000

    Update setup.sh

--- debian/setup.sh
+++ debian/setup.sh
@@ -1,1 +1,22 @@
-
+sudo apt update -y
+sudo apt install wget
+cd ~
+wget -O user.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/debian/user.sh"
+wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/debian/network.sh"
+wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/debian/packages.sh"
+wget -O docker.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/debian/docker.sh"
+# wget -O pyenv.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/debian/pyenv.sh"
+wget -O sing-box.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/debian/sing-box.sh"
+wget -O desktop.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/debian/desktop.sh"
+wget -O locale.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/debian/locale.sh"
+chmod a+x *.sh
+./user.sh
+./packages.sh
+./docker.sh
+# ./pyenv.sh
+./desktop.sh
+./sing-box.sh
+./network.sh
+./locale.sh
+cd ~
+rm -rf *.sh
