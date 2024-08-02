ba08de670bcbe8f70c5614311be37bd1339ffd68
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-08-03 02:44:54 +1000

    Update docker.sh

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -10,7 +10,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo groupadd docker
 sudo usermod -aG docker $USER
