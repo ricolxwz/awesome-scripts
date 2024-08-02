ad022e8a148f1b3c289bd646961be46b81490a6a
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-08-02 22:42:24 +1000

    Update network.sh

--- debian/network.sh
+++ debian/network.sh
@@ -35,13 +35,6 @@ read -p "请输入网关IP地址 [默认: $DEFAULT_GATEWAY]: " gateway
 gateway=${gateway:-$DEFAULT_GATEWAY}
 read -p "请输入DNS地址 [默认: $DEFAULT_DNS]: " dns
 dns=${dns:-$DEFAULT_DNS}
-read -p "是否为桌面版本? (y/n) [默认: n]: " is_desktop
-is_desktop=${is_desktop:-n}
-if [[ "$is_desktop" == "y" ]]; then
-    renderer="NetworkManager"
-else
-    renderer="networkd"
-fi
 sudo apt install openssh-client -y
 sudo apt install openssh-server -y
 sudo systemctl enable ssh
