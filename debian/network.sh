529abc4e31760c181344fbcd874a753b147c3fe4
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-08-02 23:36:49 +1000

    Update network.sh

--- debian/network.sh
+++ debian/network.sh
@@ -1,7 +1,6 @@
 DEFAULT_IP=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d'/' -f1 | head -n 1)
 DEFAULT_GATEWAY=$(ip route | grep default | awk '{print $3}')
 DEFAULT_DNS=$(ip route | grep default | awk '{print $3}')
-OLD_PROFILE=$(nmcli -t -f NAME,TYPE connection show | grep 'ethernet' | head -n 1 | cut -d: -f1)
 echo '
 alias proxy="
     export http_proxy=http://127.0.0.1:5353;
@@ -25,8 +24,6 @@ alias unproxy="
 source ~/.bashrc
 ip r
 ip a
-sudo apt install network-manager -y
-sudo systemctl restart NetworkManager
 read -p "请输入公钥: " key
 interface=$(ip -o -4 route show to default | awk '{print $5}')
 read -p "请输入静态IP地址 [默认: $DEFAULT_IP]: " ip
@@ -60,11 +57,5 @@ if [ -n "$ssh_private_key" ]; then
 else
     echo "未输入SSH私钥, 跳过保存过程"
 fi
-sudo nmcli con add type ethernet con-name static-ip ifname ${interface} ipv4.addresses ${ip}/24 ipv4.gateway ${gateway} ipv4.dns ${dns} ipv4.method manual connection.autoconnect yes
 echo "Bye Bye~, 请尝试用新的IP访问此机器"
 sudo systemctl restart ssh
-sudo rm /etc/network/interfaces
-sudo systemctl disable networking
-sudo systemctl stop networking
-sudo nmcli con up static-ip
-sudo nmcli con delete "$OLD_PROFILE"
