9b1eba484c6f2a21b4f84c42e151edd4ce7cb97c
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-08-06 15:56:51 +1000

    Update desktop.sh

--- arch/desktop.sh
+++ arch/desktop.sh
@@ -149,7 +149,30 @@ if [ "$answer" = "y" ]; then
     read -p "Install other useful programs? (y/n, default n): " extra_programs
     if [ "$extra_programs" = "y" ]; then
         yay -S --needed --noconfirm picgo zotero
-        sudo pacman -S --needed --noconfirm ksnip okular
+        sudo pacman -S --needed --noconfirm ksnip okular imwheel
+        echo "".*"
+        None,      Up,   Button4, 4
+        None,      Down, Button5, 4
+        Control_L, Up,   Control_L|Button4
+        Control_L, Down, Control_L|Button5
+        Shift_L,   Up,   Shift_L|Button4
+        Shift_L,   Down, Shift_L|Button5" > ~/.imwheelrc
+        touch ~/.config/systemd/user/imwheel.service
+        echo "[Unit]
+        Description=IMWheel
+        Wants=display-manager.service
+        After=display-manager.service
+        
+        [Service]
+        Type=simple
+        Environment=XAUTHORITY=%h/.Xauthority
+        ExecStart=/usr/bin/imwheel -d
+        ExecStop=/usr/bin/pkill imwheel
+        RemainAfterExit=yes
+        
+        [Install]
+        WantedBy=graphical.target" > ~/.config/systemd/user/imwheel.service
+        systemctl --user enable imwheel
     fi
 else
     echo "Pass."
