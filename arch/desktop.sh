4cf5ec1bd47cc35fc1edb731141d44288fbf15bc
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-08-07 23:26:27 +1000

    Update desktop.sh

--- arch/desktop.sh
+++ arch/desktop.sh
@@ -24,6 +24,7 @@ if [ "$answer" = "y" ]; then
     '    </edit>' \
     '</match>' \
     '</fontconfig>' | sudo tee /etc/fonts/local.conf > /dev/null
+    sudo fc-cache -fv
     if pacman -Qq | grep -q "^fcitx"; then
         sudo pacman -Rns $(pacman -Qq | grep "^fcitx") --noconfirm
     fi
