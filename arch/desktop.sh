0ecef627259a67fb2a5e0b7e599baaeaf2bd897b
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-08-17 14:58:30 +1000

    Update desktop.sh

--- arch/desktop.sh
+++ arch/desktop.sh
@@ -180,10 +180,10 @@ if [ "$answer" = "y" ]; then
     if [ "$extra_programs" = "y" ]; then
         read -p "ARM? (y/n, default amd64) " arm_answer
         if [ "$arm_answer" = "y" ]; then
-            sudo pacman -S --needed --noconfirm okular
+            sudo pacman -S --needed --noconfirm spectacle okular
         else
             yay -S --needed --noconfirm picgo
-            sudo pacman -S --needed --noconfirm okular
+            sudo pacman -S --needed --noconfirm spectacle okular
             # sudo pacman -S --needed --noconfirm imwheel
             # echo "\".*\"
             # None,      Up,   Button4, 1
