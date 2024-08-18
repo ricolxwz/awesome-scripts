c497559410d0305ce5e89b97699496be215b9ed7
麦旋风超好吃 <92409532+ricolxwz@users.noreply.github.com>
2024-08-18 21:31:25 +1000

    Update setup.sh

--- arch/setup.sh
+++ arch/setup.sh
@@ -4,7 +4,7 @@ cd ~
 wget -O network.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/network.sh"
 wget -O network-bare.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/network-bare.sh"
 wget -O extra-bare.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/extra-bare.sh"
-wget -O alias.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/alias.sh"
+# wget -O alias.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/alias.sh"
 wget -O packages.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/packages.sh"
 wget -O docker.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/docker.sh"
 wget -O sing-box.sh "https://raw.githubusercontent.com/ricolxwz/awesome-scripts/master/arch/sing-box.sh"
@@ -17,7 +17,7 @@ chmod a+x *.sh
 ./conda.sh
 ./sing-box.sh
 ./desktop.sh
-./alist.sh
+# ./alist.sh
 read -p "Bare Metal Arch? (y/n, default to y): " bare_metal
 if [ "$bare_metal" = "n" ]; then
     ./network.sh
