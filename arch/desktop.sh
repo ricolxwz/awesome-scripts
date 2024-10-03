5d393e8ae2abed7135a04e67f9a9736f54867070
wenzexu <92409532+ricolxwz@users.noreply.github.com>
2024-10-03 19:01:04 +1000

    Update desktop.sh

--- arch/desktop.sh
+++ arch/desktop.sh
@@ -176,45 +176,6 @@ if [ "$answer" = "y" ]; then
         # visual-studio-code-insiders-bin
     sudo chown -R $(whoami) /opt/visual-studio-code
     # sudo chown -R $(whoami) /opt/visual-studio-code-insiders
-    echo 'md5() {
-        latest_file=$(ls -t | head -n 1)
-        if [ -n "$latest_file" ]; then
-            md5_hash=$(md5sum "$latest_file" | awk '"'"'{ print $1 }'"'"')
-            extension="${latest_file##*.}"
-            if [ "$latest_file" != "$extension" ]; then
-                new_name="${md5_hash}.${extension}"
-            else
-                new_name="${md5_hash}"
-            fi
-            mv "$latest_file" "$new_name"
-            echo "Renamed $latest_file to $new_name"
-            echo -n "$new_name" | xclip -selection clipboard
-            echo "New filename copied to clipboard: $new_name"
-        else
-            echo "No files found."
-        fi
-    }
-    md5p() {
-        current_dir=$(pwd)
-        cd /home/wenzexu/Pictures/屏幕截图
-        latest_file=$(ls -t | head -n 1)
-        if [ -n "$latest_file" ]; then
-            md5_hash=$(md5sum "$latest_file" | awk '"'"'{ print $1 }'"'"')
-            extension="${latest_file##*.}"
-            if [ "$latest_file" != "$extension" ]; then
-                new_name="${md5_hash}.${extension}"
-            else
-                new_name="${md5_hash}"
-            fi
-            mv "$latest_file" /home/wenzexu/Desktop/"$new_name"
-            echo "Renamed $latest_file to $new_name"
-            echo -n "https://img.ricolxwz.io/$(date +%Y)/$(date +%m)/$new_name" | xclip -selection clipboard
-            echo "New filename copied to clipboard: $new_name"
-            cd $current_dir
-        else
-            echo "No files found."
-        fi
-    }' >> ~/.bashrc
     read -p "Install other useful programs? (y/n, default n): " extra_programs
     if [ "$extra_programs" = "y" ]; then
         read -p "ARM? (y/n, default amd64) " arm_answer
