94f873b6712d8af0556757b9de9fc642927ce63f
wenzexu <92409532+ricolxwz@users.noreply.github.com>
2024-10-03 19:01:40 +1000

    Update packages.sh

--- ubuntu/packages.sh
+++ ubuntu/packages.sh
@@ -30,6 +30,7 @@ cargo install \
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+source .bashrc
 nvm install --lts
 nvm use --lts
 echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
