0550d09e6ad3f868ab11eb4ba07aa829011faee3
wenzexu <92409532+ricolxwz@users.noreply.github.com>
2024-10-03 18:55:26 +1000

    Update packages.sh

--- ubuntu/packages.sh
+++ ubuntu/packages.sh
@@ -27,6 +27,11 @@ cargo install \
   du-dust \
   eza \
   ripgrep
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+nvm install --lts
+nvm use --lts
 echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
 echo 'export PATH="$PATH:/home/wenzexu/.cargo/bin"' >> ~/.bashrc
 echo 'alias bat="batcat"' >> ~/.bashrc
